c906e4ffd4d247721502359714fedd1f21e35f75
James <jaymz69@yahoo.com>
2022-03-15 14:15:54 -0700

    made models and saving.

---
+++ app/models/comment_model.py
@@ -0,0 +1,35 @@
+from app.models.db import db
+from datetime import datetime
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    writer_id = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    comment = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False,
+                           default=datetime.now())
+    updated_at = db.Column(db.DateTime(), nullable=False,
+                           default=datetime.now())
+
+
+# Relationships
+users = db.relationship("User", back_populates="comments")
+
+posts = db.relationship("Post", back_populates="comments")
+
+
+def to_dict(self):
+    return {
+        "id": self.id,
+        "writer": self.writer,
+        "post_id": self.post_id,
+        "comment": self.comment,
+        "created_at": self.created_at,
+        "updated_at": self.updated_at,
+        "user": {self.users.id: self.users.to_dict()},
+        "owner": self.users.username
+    }
---
+++ app/models/like_module.py
@@ -0,0 +1,32 @@
+from app.models.db import db
+from datetime import datetime
+
+
+class Likes(db.Model):
+    __tablename__ = "likes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    like_user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.db"), nullable=False)
+    like_count = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.Date, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.Date, nullable=False, default=datetime.now())
+
+
+# Relationships
+users = db.relationship("User", back_populates="likes")
+
+posts = db.relationship("Post", back_populates="likes")
+
+
+def to_dict(self):
+    return {
+        "id": self.id,
+        "like_user_id": self.like_id,
+        "post_id": self.post_id,
+        "created_at": self.created_at,
+        "updated_at": self.updated_at,
+        "user": {self.user.id: self.users.to_dict()},
+        "owner": self.users.username
+    }
