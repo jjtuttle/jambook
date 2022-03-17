968b35f5d0c25805d38b666dbd401b0596081bb7
James <jaymz69@yahoo.com>
2022-03-17 05:50:17 -0700

    saving work to go to another branch.

--- react-app/src/components/auth/SignupForm/SignUpForm.js
+++ react-app/src/components/auth/SignupForm/SignUpForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect, useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { signUp } from '../../../store/session';
 import './SignupForm.css';
 
@@ -12,7 +12,6 @@ const SignUpForm = () => {
   const [ repeatPassword, setRepeatPassword ] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-  const history = useHistory();
 
 
   const onSignUp = async (e) => {
