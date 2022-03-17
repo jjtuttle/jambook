5eef5288279965be1609940def1b1a06c0cc0585
James <jaymz69@yahoo.com>
2022-03-17 11:15:33 -0700

    saving work for lucnh time.

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,4 +1,15 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required
+import psycopg2
+# //TODO:  need post form import to
+from app.models import Post, db
+from datetime import datetime
+
+# //todo —————————————————————————————————————————————————————————————————————
 
 
-# //todo ——————————————————————————————————————————————————————————————————————
+
+
+
+
 
