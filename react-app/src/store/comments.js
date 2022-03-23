5f0499c94fac370bae78f52a7c0832bf27d750ec
James <jaymz69@yahoo.com>
2022-03-23 07:32:12 -0700

    Have comments back to where displaying on posts BUT for every post, need to still filter on c.post_id === p.id

--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -1,6 +1,6 @@
 const CREATE_COMMENT = 'comment/create';
-const GET_ALL_COMMENT = 'comment/get_all';
-const GET_ONE_COMMENT = 'comment/get_one';
+const GET_ALL_COMMENT_BY_ID = 'comment/get_all_by_id';
+const GET_ALL_COMMENTS = 'comment/get_all_comments';
 const UPDATE_COMMENT = 'comment/update';
 const DELETE_COMMENT = 'comment/delete';
 
@@ -10,8 +10,8 @@ const DELETE_COMMENT = 'comment/delete';
 // ===========================================================================
 
 const create = (comment) => ({ type: CREATE_COMMENT, comment });
-const getAll = (comment) => ({ type: GET_ALL_COMMENT, comment });
-const getOne = (comment) => ({ type: GET_ONE_COMMENT, comment });
+const getAllCommentById = (comment) => ({ type: GET_ALL_COMMENT_BY_ID, comment });
+const getAllCommentsNoId = (comment) => ({ type: GET_ALL_COMMENTS, comment });
 const update = (comment) => ({ type: UPDATE_COMMENT, comment });
 const destroy = (comment) => ({ type: DELETE_COMMENT, comment });
 
@@ -39,13 +39,30 @@ export const createComment = (payload) => async (dispatch) => {
 };
 
 
-// Get comments
-export const getComment = (id) => async (dispatch) => {
-    const response = await fetch(`/api/comments/${id}`, { method: 'GET' });
-console.log("id................", id);
+
+
+// Get all 
+export const getAllComments = () => async (dispatch) => {
+
+    const response = await fetch(`/api/comments`, { method: 'GET' });
     if (response.ok) {
         const comment = await response.json();
-        dispatch(getAll(comment));
+        dispatch(getAllCommentsNoId(comment));
+        return comment;
+    }
+    return response;
+}
+
+
+
+
+// Get comments by ID
+export const getComment = (id) => async (dispatch) => {
+
+    const response = await fetch(`/api/comments/${id}`, { method: 'GET' });
+    if (response.ok) {
+        const comment = await response.json();
+        dispatch(getAllCommentById(comment));
         return comment;
     }
     return response;
@@ -92,14 +109,15 @@ const commentReducer = (state = {}, action) => {
             newState[ action.comment.id ] = action.comment;
             return newState;
         };
-        case GET_ALL_COMMENT: {
-            const newState = { ...state };
-            action.comment[ 'all_comments' ].forEach((comment) => newState[ comment.id ] = comment);
+        case GET_ALL_COMMENT_BY_ID: {
+            const newState = {}; //...state
+            action.comment[ 'all_comment' ].forEach((comment) => newState[ comment.id ] = comment);
             return newState;
         };
-        case GET_ONE_COMMENT: {
-            const newState = state;
-            newState[ action.comment.id ] = action.comment;
+        case GET_ALL_COMMENTS: {
+            const newState = {}; //state;
+            // newState[ action.comment.id ] = action.comment;
+            action.comment[ 'all_comments' ].forEach((comment) => newState[ comment.id ] = comment);
             return newState;
         };
         case UPDATE_COMMENT: {
