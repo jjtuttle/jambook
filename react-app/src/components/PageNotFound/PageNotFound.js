814133efc9aa46fae1e25549b9ebf254f2fd5bb7
James <jaymz69@yahoo.com>
2022-03-28 06:01:08 -0700

    fixed 404 page with logo and style.

--- react-app/src/components/PageNotFound/PageNotFound.js
+++ react-app/src/components/PageNotFound/PageNotFound.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './PageNotFound.css';
 import pageNotFound from '../../images/page-not-found-cassette-unspooled.jpg';
-
+import logo from '../../images/jambook-logo-blue.png'
 
 const PageNotFound = () => {
     // todo ————————————————————————————————————————————————————————————————————————
     return (
         <>
-            <div style={{top:'40vh'}}>
-                <h1>404 PAGE NOT FOUND</h1>
+            <div>
+                
+                <img className="logo-page-not-found" src={logo} alt="logo"
+                    style={{
+                        width: '150px', height: '60px', position: 'absolute',
+                        top: '17.5vh', right: '45vw', paddingBottom:'10px'
+                    }}
+                ></img>
+                <h1 style={{marginTop:'100px'}}>404 PAGE NOT FOUND</h1>
                 <h3>Rewind me back... Hit the back button </h3>
             </div>
 
